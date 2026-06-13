bd57736ad722285c766730640bc8fe334452e4dc
T.I.Q.S. <daquietstorm22@gmail.com>
2026-06-12 18:47:47 -0700

    eighth commit

--- 2-backend.tf
+++ 2-backend.tf
@@ -1,19 +1,19 @@
 terraform {
   backend "s3" {
-    bucket  = "your-bucket-name"          # Name of the S3 bucket
-    key     = "your-state-file.tfstate"   # The name of the state file in the bucket
-    region  = "us-east-1"                 # Choose your region
-    encrypt = true                        # Enable server-side encryption (optional but recommended)
+    bucket  = "tiqs-tf-state-files"    # Name of the S3 bucket
+    key     = "aws-to-gcp-vpn.tfstate" # The name of the state file in the bucket
+    region  = "us-east-1"              # Choose your region
+    encrypt = true                     # Enable server-side encryption (optional but recommended)
   }
 
   required_providers {
     google = {
       source  = "hashicorp/google",
-      version = "~> 6.36.0"
+      version = "~> 6.0"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"                  # Use latest version if possible
+      version = "~> 6.0" # Use latest version if possible
 
     }
   }
